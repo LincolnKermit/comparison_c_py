3629befaa6e4a58b83ad4cbd6fd3c2c0a95183c6
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2024-03-11 20:54:10 +0100

    Counter in C/Python until 1 Million

---
+++ counter.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(){
+    for (int i = 0; i < 1000001; i++){
+        printf("The counter is %d\n", i);
+    }
+}
---
+++ counter.py
@@ -0,0 +1,4 @@
+i = 0
+for i in range(1000001):
+    print(i)
+
